0d24ecc0d3ae331558655f613e69f2c016f6e73f
sanaiylatariq <sanaiylatariq517@gmail.com>
2024-06-29 23:15:38 +0500

    Add files via upload

---
+++ sunny.js
@@ -0,0 +1,84 @@
+//const name = "sunny"
+//const repscore = 5
+//console.log(name + repscore);
+//const name = "sunny"
+//const repscore = 5
+//const repo = true
+//const rep = null
+//console.log(name +   repscore +   repo +   rep +  " value");  
+
+//   life lesson  string and var ko attach karny k lye use backticks on the left of numerical 1 key on top left cornrer of keyboard
+
+   // backticks on left of 1       // 
+//let dani = "khota"
+//let age = 5
+//let dimag = "khali"
+//console.log(`hello dani is ${dani} and his age is ${age} and his dimag is ${dimag}`);
+// let allah = "akbar"
+// let kalma = "wahid"
+// let akhrenabi = 1
+//console.log(`allah o ${allah} allah is ${kalma} and there is only ${akhrenabi} khatim ul anbiya`);
+
+
+
+//   new string daalmny sy tabulated form mn sb aata hay
+// const nam1 = new String('sanaiyla')
+
+
+//    key access karna
+//console.log(nam1[7]);
+//console.log(nam1.__proto__);
+//console.log(nam1.length);
+//console.log(nam1.lastIndexOf());
+//console.log(nam1.toString());
+//console.log(nam1.fontsize());
+//console.log(nam1.repeat());
+//console.log(nam1.codePointAt());
+//console.log(nam1.concat());
+//console.log(nam1.trim());
+//console.log(nam1.padStart());
+
+//console.log(sunny);
+//console.log(typeof sunny);
+//console.log(sunny[4]);
+//console.log(sunny.endsWith);
+//console.log(sunny.length);
+// console.log(sunny.charAt(3));
+// console.log(sunny.indexOf(`a`));
+// console.log(sunny.indexOf(`n`));
+// const sunny= new String(`sanaiyla`)
+// let sunny1 = sunny.substring(0,5)
+// console.log(sunny1);
+// console.log(sunny.slice(-7,5));
+// const sunny2 = new String(`       k  a    t        o       `)
+// console.log(sunny2.trim);
+// console.log(sunny.substring(0,4));
+// console.log(sunny.indexOf(`y`));
+
+// console.log(sunny.slice(-7,4));
+// console.log(sunny);
+
+
+
+  // trim removes extra spaces // add nothing in trim only blank brackets//
+// console.log(sunny.trimEnd());
+// console.log(sunny.trimStart());
+// console.log(sunny.trim());
+// let hello = new String(`helooooo`)
+// console.log(hello.toString());
+// console.log(hello);
+
+// let sunny = new String(`   sanaiyla      `)
+
+// replace k lye var.replace within brackets and backticks type what to replace and the replacement//
+
+const url = "http://www.sunny20%.com"
+console.log(url.replace(`20%`, `.google`));
+  // include to know whether that particlar keyword is present within code or not use brackets and backticks
+console.log(url.includes(`sunny`));
+console.log(url.includes(`google`));
+
+
+// split property is used to split , use dot and bracckets backticks along with the word u want to split//
+const nam1 = new String(`sunny-hc-com`)
+console.log(nam1.split(`-`));
